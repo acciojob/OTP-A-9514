a9372c76c5d99933e363da6cc3d35c3724db8c57
Aishwarya <142329750+A-9514@users.noreply.github.com>
2024-04-08 20:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
-
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+const app = require("./index.js");
+app.listen(3000, () => {
+  console.log('server started');
 });
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
